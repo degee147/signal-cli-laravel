626faae88dc95090975394074861d467e3f0411e
Ken Waribo <degee147@gmail.com>
2023-04-26 11:23:17 +0100

    test

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,10 @@ Route::prefix('/')->controller(SignalController::class)->group(function () {
     Route::post('/register', 'register')->name('register');
     Route::post('/verify', 'verify')->name('verify');
     Route::get('/unregister', 'unregister')->name('unregister');
-    Route::get('version', 'version')->name('version');
+    Route::get('version', function (Request $request) {
+        return "working";
+    });
+    // Route::get('version', 'version')->name('version');
     Route::get('receive', 'receive')->name('receive');
     Route::post('sendmessage', 'sendmessage')->name('sendmessage');
     Route::post('command', 'command')->name('command');
